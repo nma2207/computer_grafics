6148b572372d74722e17b78d7d0ba12dd59963b1
nma2207 <nma2207@gmail.com>
2016-10-10 17:45:46 +0300

    Write function DrawLine

--- DZ2.py
+++ DZ2.py
@@ -6,16 +6,10 @@ def event(evt, val):
     if evt == Draw.ESCKEY :
         Draw.Exit()
         return
-
-def gui():
-	sw=False
-	glClearColor(0,0,0,1)
-	global width
+def drawline(x0,y0,x1,y1,width):
 	glPointSize(width)
-	glClear(BGL.GL_COLOR_BUFFER_BIT)
 	glColor3f(0,0,1)
-	glBegin(GL_POINTS)
-	global x0,x1,y0,y1
+
 	if x0>x1:
 		x0,x1=x1,x0
 		y0,y1=y1,y0
@@ -26,16 +20,15 @@ def gui():
 	if y1<y0:
 		sign=-1
 		dy=-dy
+	sw=False
 	if dy>dx:
 		dy,dx=dx,dy
 		sw=True
-
-
 	err=0
 	derr=dy
+	glBegin(GL_POINTS)
 	for x in range(dx+1):
 		px,py=0,0
-
 		if sw==True:
 			px,py=sign*y+x0,sign*x+y0
 		else:
@@ -46,6 +39,14 @@ def gui():
 			y+=sign
 			err-=dx
 	glEnd()
+
+
+
+def gui():
+	glClearColor(0,0,0,1)
+	glClear(BGL.GL_COLOR_BUFFER_BIT)
+	global width,x0,x1,y0,y1
+	drawline(x0,y0,x1,y1,width)
 
 print 'write points and width,please'
 x0=int(raw_input('x0='))
