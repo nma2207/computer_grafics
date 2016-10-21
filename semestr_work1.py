2baef67bb08384fc3a463aa579da4f82ae80ae9e
nma2207 <nma2207@gmail.com>
2016-10-21 06:33:46 +0300

    add changeR()

--- semestr_work1.py
+++ semestr_work1.py
@@ -7,14 +7,23 @@ import math
 x_circle=[]
 y_circle=[]
 
+def changeR():
+	global R
+	R+=1
+	if R>1000:
+		Draw.Exit()
+	time.sleep(0.1)
+	Draw.Redraw(1)
 def circle_coords(R,center):
 	global x_circle, y_circle
+	del x_circle[0:]
+	del y_circle[0:]
 	n=100
 	dx=2*R/n
-	for i in range(n+1):
+	for i in range(n):
 		x_circle.append(dx*i+center[0]-R)
 		y_circle.append((R*R-(dx*i-R)**2)**0.5+center[1])
-	for i in range(n+1):
+	for i in range(n):
 		x_circle.append(R-dx*i+center[0])
 		y_circle.append(-(R*R-(dx*i-R)**2)**0.5+center[1])
 
@@ -32,7 +41,10 @@ def vector(x0,x1):
 
 def dPoint(R, n,mid,x0):
 	if n[1]!=0:
-		sign=-(n[0]/n[1])/math.fabs(n[0]/n[1])
+		if n[0]==0:
+			sign=1
+		else:
+			sign=-(n[0]/n[1])/math.fabs(n[0]/n[1])
 		len=R*R-(dist_p_to_p_sqr(mid,x0))
 		dx=sign*(((len*n[1]*n[1])/(n[0]**2+n[1]**2))**0.5)
 		dy=((len*n[0]*n[0])/(n[0]**2+n[1]**2))**0.5
@@ -54,19 +66,18 @@ def gui():
 	global x_circle,y_circle,x0,x1,R
 	glClearColor(0,0,0,1)
  	glClear(GL_COLOR_BUFFER_BIT)
-	glLineWidth(1)
+	glLineWidth(2)
 	glColor3f(1.0,0,0)
 	glBegin(GL_LINE_LOOP)
 	counter(x0,x1,R,1)
 	for i in range(len(x_circle)):
 		glVertex2f(x_circle[i],y_circle[i])
-		print x_circle[i],y_circle[i]
 	glEnd()
+	glColor3f(0,0,2)
 	glBegin(GL_LINE_LOOP)
 	counter(x0,x1,R,-1)
 	for i in range(len(x_circle)):
 		glVertex2f(x_circle[i],y_circle[i])
-		print x_circle[i],y_circle[i]
 	glEnd()
 	glColor3f(0, 1.0, 0.5)
 	glPointSize(5)
@@ -74,6 +85,7 @@ def gui():
 	glVertex2f(x0[0],x0[1])
 	glVertex2f(x1[0],x1[1])
 	glEnd()
+	changeR()
 
 
 x0=[float(i) for i in raw_input("Write first point\n").split()]
@@ -85,9 +97,5 @@ while len(x1)!=2:
 R=float(raw_input("Write radius of circle\n"))
 if(dist_p_to_p_sqr(x0,x1)>4*R*R):
 	print 'Your coord is wrong, it is impossible'
-	n=10
-	dx=2*5/10
-	for i in range(n+1):
-		print i*dx-5
 else:
 	Draw.Register(gui, event,None)
