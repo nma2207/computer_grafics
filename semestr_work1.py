b956e63f289f2715e82f4dce1cc5229e93fb3063
nma2207 <nma2207@gmail.com>
2016-10-26 06:28:27 +0300

    Add block for data

--- semestr_work1.py
+++ semestr_work1.py
@@ -6,6 +6,7 @@ import math
 
 x_circle=[]
 y_circle=[]
+is_draw=False
 
 def changeR():
 	global R
@@ -29,8 +30,13 @@ def circle_coords(R,center):
 
 
 def event(evt, val):
+	global is_draw
 	if evt == Draw.ESCKEY :
 		Draw.Exit()
+	if evt==Draw.RKEY:
+		is_draw=False
+		Draw.Register(gui, event,button_event)
+
 		return
 def mid(x0,x1):
 	return [(x0[0]+x1[0])/2, (x1[1]+x0[1])/2]
@@ -63,39 +69,72 @@ def counter(x0,x1,R,sign):
 def dist_p_to_p_sqr(x0,x1):
 	return (x1[0]-x0[0])**2+(x1[1]-x0[1])**2
 def gui():
-	global x_circle,y_circle,x0,x1,R
-	glClearColor(0,0,0,1)
- 	glClear(GL_COLOR_BUFFER_BIT)
-	glLineWidth(2)
-	glColor3f(1.0,0,0)
-	glBegin(GL_LINE_LOOP)
-	counter(x0,x1,R,1)
-	for i in range(len(x_circle)):
-		glVertex2f(x_circle[i],y_circle[i])
-	glEnd()
-	glColor3f(0,0,2)
-	glBegin(GL_LINE_LOOP)
-	counter(x0,x1,R,-1)
-	for i in range(len(x_circle)):
-		glVertex2f(x_circle[i],y_circle[i])
-	glEnd()
-	glColor3f(0, 1.0, 0.5)
-	glPointSize(5)
-	glBegin(GL_POINTS)
-	glVertex2f(x0[0],x0[1])
-	glVertex2f(x1[0],x1[1])
-	glEnd()
-	changeR()
+	global x_circle,y_circle,x0,x1,R, is_draw
+	if is_draw==False:
+		Draw.PushButton("Write points and R",1,100,100,200,50,"Write point and raduis,please")
+		glRasterPos2f(100.0,200.0)
+		Draw.Text("Press ESC to exit and R to restart")
+
+	else:
+		glClearColor(0,0,0,1)
+ 		glClear(GL_COLOR_BUFFER_BIT)
+		glLineWidth(2)
+		glColor3f(1.0,0,0)
+		glBegin(GL_LINE_LOOP)
+		counter(x0,x1,R,1)
+		for i in range(len(x_circle)):
+			glVertex2f(x_circle[i],y_circle[i])
+		glEnd()
+		glColor3f(0,0,2)
+		glBegin(GL_LINE_LOOP)
+		counter(x0,x1,R,-1)
+		for i in range(len(x_circle)):
+			glVertex2f(x_circle[i],y_circle[i])
+		glEnd()
+		glColor3f(0, 1.0, 0.5)
+		glPointSize(5)
+		glBegin(GL_POINTS)
+		glVertex2f(x0[0],x0[1])
+		glVertex2f(x1[0],x1[1])
+		glEnd()
+		changeR()
+
+def button_event(evt):
+	global x0,x1,R, is_draw
+  	if evt == 1:
+
+		x_start=Draw.Create(0)
+		y_start=Draw.Create(0)
+		x_end=Draw.Create(0)
+		y_end=Draw.Create(0)
+		R_circle=Draw.Create(0)
+		block=[]
+		block.append(("X0= ",x_start,0,800))
+		block.append(("Y0= ",y_start,0,800))
+		block.append(("X1= ",x_end,0,800))
+		block.append(("Y1= ",y_end,0,800))
+		block.append(("R= ",R_circle,0,1000))
+		retVal=Draw.PupBlock("Line coords",block)
+		x0=[x_start.val, y_start.val]
+		x1=[x_end.val,y_end.val]
+		R=R_circle.val
+		if(dist_p_to_p_sqr(x0,x1)>4*R*R):
+			glRasterPos2f(100.0,200.0)
+			Draw.Text('Your coord is wrong, it is impossible')
+		else:
+			is_draw=True
+			Draw.Redraw(1)
+		return
 
 
-x0=[float(i) for i in raw_input("Write first point\n").split()]
-while len(x0)!=2:
-	x0=[float(i) for i in raw_input("Write for  first point 2 coords\n").split()]
-x1=[int(i) for i in raw_input("Write second point\n").split()]
-while len(x1)!=2:
-	x1=[int(i) for i in raw_input("Write for  second point 2 coords\n").split()]
-R=float(raw_input("Write radius of circle\n"))
-if(dist_p_to_p_sqr(x0,x1)>4*R*R):
-	print 'Your coord is wrong, it is impossible'
-else:
-	Draw.Register(gui, event,None)
+#x0=[float(i) for i in raw_input("Write first point\n").split()]
+#while len(x0)!=2:
+#	x0=[float(i) for i in raw_input("Write for  first point 2 coords\n").split()]
+#x1=[int(i) for i in raw_input("Write second point\n").split()]
+#while len(x1)!=2:
+#	x1=[int(i) for i in raw_input("Write for  second point 2 coords\n").split()]
+#R=float(raw_input("Write radius of circle\n"))
+#if(dist_p_to_p_sqr(x0,x1)>4*R*R):
+#	print 'Your coord is wrong, it is impossible'
+#else:
+Draw.Register(gui, event,button_event)
