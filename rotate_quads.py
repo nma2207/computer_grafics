9e38d01c0545f2209390cea915e7c3c578f1b669
nma2207 <nma2207@gmail.com>
2016-11-04 13:11:30 +0300

    Add change size of quad

--- rotate_quads.py
+++ rotate_quads.py
@@ -11,21 +11,28 @@ import random
 changeColor=False
 c1,c2,c3=0,0,0
 colorA=0.0
+signK=1
+signA=1
+rotateA=0.0
 A=0.0
 k=1.0;
 
 def changeA():
 	da=.2
-	global changeColor,A,k,colorA;
+	dk=da/(2*math.pi)
+	global changeColor,A,k,colorA,signK,rotateA,signA;
 	if A>2*math.pi:
 		A-=math.pi*2
 	if colorA>math.pi/2:
 		changeColor=True
 		colorA-=math.pi/2
+	if k>2 or k<1:
+		signK*=-1
 	A+=da
 	colorA+=da
 	time.sleep(0.1)
-	k=1;
+	k+=signK*dk;
+	#print k
 	Draw.Redraw(1)
 
 
@@ -45,14 +52,14 @@ def event(evt, val):
 
 def randomColor():
 	global changeColor,c1,c2,c3;
-	print c1,c2,c3
+	#print c1,c2,c3
 	if changeColor==True:
 		c1,c2,c3=random.random(),random.random(),random.random()
 		changeColor=False;
 	glColor3f(c1,c2,c3)
 
 def gui():
-	global k,A,pc0,pc1,pc2,pc3,mid
+	global k,A,pc0,pc1,pc2,pc3,mid,signA
 	glClearColor(1.0,1.0,1.0,1) # background color
 	glClear(GL_COLOR_BUFFER_BIT) # clear image buffer
 	glColor3f(0,0,0)
@@ -70,7 +77,7 @@ def gui():
 	glVertex2f(*pn2)
 	glVertex2f(*pn3)
 	glEnd()
-	#print (pn0[0]-pn1[0])**2+(pn0[1]-pn1[1])**2
+	print ((pn0[0]-pn1[0])**2+(pn0[1]-pn1[1])**2)**0.5
 	changeA()
 
 point =[float(i) for i in raw_input("write coord of left top angle:\n").split()]
