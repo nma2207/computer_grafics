173adea69b2fb16917618a8285e49688707c5c82
nma2207 <nma2207@gmail.com>
2016-12-06 20:26:47 +0300

    coorect mitoz teni

--- semestr_work2.py
+++ semestr_work2.py
@@ -10,31 +10,45 @@ msh2=ob2.getData(False,True)
 fs2=msh2.faces
 
 a=0
+sign=1
 r1=2
 r2=2
-buffP=Buffer(GL_FLOAT,4,[0.,0.,20,1.]) #initial position of LIGHT0 (proj coords)
+buffP=Buffer(GL_FLOAT,4,[4.,20.,10,1.]) #initial position of LIGHT0 (proj coords)
 def changeA():
-	global a
-	if(a<r1+r2):
-		a+=0.01
-		time.sleep(0.01)
-		Draw.Redraw(1)
+	global a,sign
+	d=0.03
+	if(a<0 or a>r1+r2):
+		sign*=-1
+	a+=sign*d
+	#buffP[0] +=1 #   changes  coords after each step
+	#buffP[1] +=2
+	time.sleep(0.01)
+	Draw.Redraw(1)
+
 def key(evt,val):
 	if evt==Draw.ESCKEY: Draw.Exit()
 def plane():
 	glBegin(GL_QUADS)
-	glVertex3f(-20,-20,-1)
-	glVertex3f(20,-20,-1)
-	glVertex3f(20,20,-1)
-	glVertex3f(-20,20,-1) #  plane where we see  shadow
+	glVertex3f(-1000,-1000,0)
+	glVertex3f(1000,-1000,0)
+	glVertex3f(1000,1000,0)
+	glVertex3f(-1000,1000,0) #  plane where we see  shadow
 	glEnd()
+
 def createMatr():	# creates shadow matrix that transforms the original triangles into its shadow
 	glLightfv(GL_LIGHT0,GL_POSITION,buffP)
 	glEnable(GL_LIGHT0)
-	mmat=Buffer(GL_FLOAT,16,[buffP[2],0, 0,0 , 0,buffP[2],0,0, -buffP[0],-buffP[1],0, -1,  0,0,0, buffP[2]])
+	mmat=Buffer(GL_FLOAT,16,[buffP[2],0, 4,10 , 0,buffP[2],0,4, -buffP[0],-buffP[1],10, -1,  10,4,10, buffP[2]])
 	glMultMatrixf(mmat)
+	#amb=Buffer(GL_FLOAT,4,(0,0,1,1.))
+	#glLightfv(GL_LIGHT0,GL_AMBIENT,amb)
+	#dif=Buffer(GL_FLOAT,4,(0.,0.,0.,1.))
+	#glLightfv(GL_LIGHT0,GL_DIFFUSE,dif)
+	#spec=Buffer(GL_FLOAT,4,(0,0,0 ,1))
+	#glLightfv(GL_LIGHT0,GL_SPECULAR,spec)
+
 def gui():
-	glClearColor(0,0,0,1)
+	glClearColor(1,1,1,1)
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 	glEnable(GL_DEPTH_TEST)
 	glViewport(0,0,1000,1000)
@@ -42,22 +56,29 @@ def gui():
 	glLoadIdentity()
 #	glFrustum(-100,100,-100,100,30,500)
 	glOrtho(-10,10,-10,10,-500,500)
+	#glTranslatef(0,-5,5)
 	glEnable(GL_LIGHTING)
-	pos=Buffer(GL_FLOAT,4,(-2,2,7,1))
+	pos=Buffer(GL_FLOAT,4,(2,2,5,1))
 	glLightfv(GL_LIGHT0,GL_POSITION,pos)
-	dif=Buffer(GL_FLOAT,4,(0.5,0.,0.,1.))
+	dif=Buffer(GL_FLOAT,4,(0.5,0.,0.,1))
 	glLightfv(GL_LIGHT0,GL_DIFFUSE,dif)
-	spec=Buffer(GL_FLOAT,4,(0.5,.5,0.5 ,1))
-	glLightfv(GL_LIGHT0,GL_SPECULAR,spec)
-	#amb=Buffer(GL_FLOAT,4,(0,0,1,1.))
-	#glLightfv(GL_LIGHT0,GL_AMBIENT,amb)
+	#spec=Buffer(GL_FLOAT,4,(0.5,.5,0.5 ,1))
+	#glLightfv(GL_LIGHT0,GL_SPECULAR,spec)
+	amb=Buffer(GL_FLOAT,4,(0,0,1,1.))
+	glLightfv(GL_LIGHT0,GL_AMBIENT,amb)
+	glEnable(GL_COLOR_MATERIAL);
+	glColor3f(0,1,0)
+	glTranslatef(0,0,10)
 	circle1()
 	circle2()
-	#createMatr()
-	#glColor3f(0,0,0)
-	#circle1()
-	#circle2()
-	#glColor3f(0,0,0)
+	#glTranslatef(1,1,-10)
+
+	createMatr()
+	#glTranslatef(8,2.8,0)
+	glColor3f(.1,.1,0.1)
+	circle1()
+	circle2()
+	#glColor3f(0,0,0
 
 	changeA()
 def createMatr():	# creates shadow matrix that transforms the original triangles into its shadow
@@ -78,7 +99,7 @@ def circle1():
 	glPushMatrix()
 	glTranslatef(-5-a/2,0,0)
 	#glRotatef(20,1,1,1)
-	glColor3f(0,1,0)
+	#glColor3f(0,1,0)
 	for ff in fs1:
 
 		if len(ff.verts)==4:
@@ -98,7 +119,7 @@ def circle1():
 def circle2():
 	glPushMatrix()
 	glTranslatef(-5+a/2,0,0)
-	glColor3f(1,0,0)
+	#`glColor3f(1,0,0)
 	for ff in fs2:
 		if len(ff.verts)==4:
 
